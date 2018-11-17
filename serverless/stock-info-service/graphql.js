88e5163528fe8cb71558e20b9a571cb17b73bb2f
Christopher Bartling <chris.bartling@gmail.com>
2018-11-17 00:07:58 -0600

    Moving hello resolver to its own file.

--- serverless/stock-info-service/graphql.js
+++ serverless/stock-info-service/graphql.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import { ApolloServer, gql } from 'apollo-server-lambda';
+import {hello} from './graphql/resolvers/hello';
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -11,7 +12,7 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
     Query: {
-        hello: () => 'Hello world!',
+        hello,
     },
 };
 
@@ -30,7 +31,7 @@ const server = new ApolloServer({
             'editor.cursorShape': 'line', // possible values: 'line', 'block', 'underline'
             'editor.fontSize': 16,
             'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
-            'editor.theme': 'dark', // possible values: 'dark', 'light'
+            'editor.theme': 'light', // possible values: 'dark', 'light'
             'editor.reuseHeaders': true,
             'request.credentials': 'omit', // possible values: 'omit', 'include', 'same-origin'
             'tracing.hideTracingResponse': true,
