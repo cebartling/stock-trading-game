9f3c8d68c6b2a43862170a2292ea67f5689983f3
Christopher Bartling <chris.bartling@gmail.com>
2018-11-17 19:52:56 -0600

    Adding more AWS infrastructure.

--- stock-trading-game-aws/main.tf
+++ stock-trading-game-aws/main.tf
@@ -4,7 +4,92 @@ terraform {
 
 provider "aws" {
   region = "${var.aws_region}"
+
+  // profile = "some-profile"
 }
+
+######################################################################################################################
+## Variables used across all modules
+######################################################################################################################
+locals {
+  production_availability_zones = [
+    "us-east-1a",
+    "us-east-1b",
+  ]
+}
+
+module "networking" {
+  source      = "./modules/networking"
+  environment = "production"
+  vpc_cidr    = "10.0.0.0/16"
+
+  public_subnets_cidr = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+  ]
+
+  private_subnets_cidr = [
+    "10.0.10.0/24",
+    "10.0.20.0/24",
+  ]
+
+  region             = "${var.aws_region}"
+  availability_zones = "${local.production_availability_zones}"
+  key_name           = "production_key"
+}
+
+//module "s3" {
+//  source = "./modules/s3"
+//  environment = "production"
+//  aws_bucket = "${var.aws_bucket}"
+//}
+
+
+//module "stock_info_service" {
+//  source      = "./modules/stock_info_service"
+//  application = "${var.application}"
+//  aws_region  = "${var.aws_region}"
+//  environment = "${var.environment}"
+//  provisioner = "${var.provisioner}"
+//}
+
+
+//module "worker_lambda" {
+//  source = "./modules/worker_lambda"
+//  environment = "production"
+//  aws_ecs_cluster_name = "${module.ecs.aws_ecs_cluster_name}"
+//  worker_task_definition_arn = "${module.ecs.worker_task_definition_arn}"
+//  workflow_queue_url = "${module.workflow_queue.workflow_queue_url}"
+//  private_subnets_id = "${module.networking.private_subnets_id}"
+//  workflow_bucket_arn = "${module.s3.workflow_bucket_arn}"
+//}
+
+
+//module "workflow_queue" {
+//  source = "./modules/workflow_queue"
+//  environment = "production"
+//}
+
+
+//module "ecs" {
+//  source = "./modules/ecs"
+//  environment = "production"
+//  vpc_id = "${module.networking.vpc_id}"
+//  availability_zones = "${local.production_availability_zones}"
+//  repository_name = "worker-pattern-spike/production"
+//  subnets_ids = [
+//    "${module.networking.private_subnets_id}"
+//  ]
+//  public_subnet_ids = [
+//    "${module.networking.public_subnets_id}"
+//  ]
+//  security_groups_ids = [
+//    "${module.networking.security_groups_ids}"
+//  ]
+//  workflow_queue_url = "${module.workflow_queue.workflow_queue_url}"
+//  aws_region = "${var.aws_region}"
+//}
+
 
 //module "rds_postgresql" {
 //  source = "./modules/rds_postgresql"
@@ -14,10 +99,11 @@ provider "aws" {
 //  provisioner = "${var.provisioner}"
 //}
 
-module "stock_info_service" {
-  source      = "./modules/stock_info_service"
-  application = "${var.application}"
-  aws_region  = "${var.aws_region}"
-  environment = "${var.environment}"
-  provisioner = "${var.provisioner}"
-}
+
+//module "api_gateway" {
+//  source = "./modules/api_gateway"
+//  environment = "production"
+//  workflow_lambda_function_arn = "${module.worker_lambda.workflow_lambda_function_arn}"
+//  workflow_lambda_function_invoke_arn = "${module.worker_lambda.workflow_lambda_function_invoke_arn}"
+//}
+
--- stock-trading-game-aws/variables.tf
+++ stock-trading-game-aws/variables.tf
@@ -15,3 +15,51 @@ variable "provisioner" {
 variable "aws_region" {
   description = "The AWS Region to use for provisioning all resources in this solution."
 }
+
+variable "app_name" {
+  default = "ECSPOVRayWorker"
+}
+
+//variable "app_env" {
+//    description = "Application execution environment"
+//}
+
+variable "aws_profile" {
+  default = "default"
+}
+
+variable "aws_bucket" {
+  default = "stock-trading-game-bucket-"
+}
+
+variable "sqs_queue_name" {
+  default = "StockTradingGameWorkerQueue"
+}
+
+variable "lambda_function_name" {
+  default = "ecs-worker-launcher"
+}
+
+variable "ecs_task_name" {
+  default = "ECSPOVRayWorkerTask"
+}
+
+variable "bucket_permission_sid" {
+  default = "ECSPOVRayWorkerPermission"
+}
+
+variable "wait_time" {
+  default = 5
+}
+
+variable "retries" {
+  default = 5
+}
+
+variable "lambda_execution_role_name" {
+  default = "ECSPOVRayWorker-Lambda-Execution-Role"
+}
+
+variable "lambda_execution_role_policy_name" {
+  default = "AWSLambdaExecutionPolicy"
+}
