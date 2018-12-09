4d19a795adedd1258488316c54cdcf4bcb45073e
Christopher Bartling <chris.bartling@gmail.com>
2018-12-09 07:36:44 -0600

    Adding Apolloo Client to the mix.

--- stock-trading-game-vue/src/views/Home.vue
+++ stock-trading-game-vue/src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+      <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
---
+++ stock-trading-game-vue/src/views/StockInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StockInfo'
+}
+</script>
+
+<style scoped>
+
+</style>
